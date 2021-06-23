4adc460829986f33da9dd85bf7d88aab3d4fc3af
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-06-23 13:43:50 -0500

    footer component rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import About from "./pages/About";
+// import Skills from "./pages/Skills";
+// import Projects from "./pages/Projects";
+// import Contact from "./pages/Contact";
+// import Navbar from "./components/Navbar";
+// import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
 import './App.css';
 
+// function App() {
+
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <Wrapper>
+//           <Route exact path="/" component={About} />
+//           <Route exact path="/about" component={About} />
+//           <Route exact path="/skills" component={Skills} />
+//           <Route exact path="/projects" component={Projects} />
+//           <Route exact path="/contact" component={Contact} />
+//         </Wrapper>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
 function App() {
-
-
   return (
-    <>
-
-    </>
+  <Footer />
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer class="page-footer blue-grey darken-2">
+            <div class="container">
+                <p class="center-align">© 2021 <a aria-label="Email Pam" href="mailto:pamela.jk.hsu@gmail.com" target="_blank" rel="noreferrer" data-tooltip="Email Pam" class="waves waves-effect waves-light teal-text text- darken-2">Pamela Hsu</a></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
