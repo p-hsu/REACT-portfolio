c3dcfa201cd6b0c28d3196b47d4a46783e24575a
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-06-23 15:05:30 -0500

    edited footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 function Footer() {
     return (
-        <footer class="page-footer blue-grey darken-2">
-            <div class="container">
-                <p class="center-align">© 2021 <a aria-label="Email Pam" href="mailto:pamela.jk.hsu@gmail.com" target="_blank" rel="noreferrer" data-tooltip="Email Pam" class="waves waves-effect waves-light teal-text text- darken-2">Pamela Hsu</a></p>
+        <footer className="page-footer blue-grey darken-2">
+            <div className="container">
+                <p className="center-align">© 2021 <a aria-label="Email Pam" href="mailto:pamela.jk.hsu@gmail.com" target="_blank" rel="noreferrer" data-tooltip="Email Pam" className="waves waves-effect waves-light teal-text text- darken-2">Pamela Hsu</a></p>
             </div>
         </footer>
     );
