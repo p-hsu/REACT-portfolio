45b0891009e969452a1eadef080a742e0ba8b07b
Pam Hsu <pamela.jk.hsu@gmail.com>
2021-06-23 15:08:10 -0500

    edited class to className

--- src/components/HomeContainer/index.js
+++ src/components/HomeContainer/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function HomeContainer() {
     return (
         <div className="container">
-            <h2 className="white-text">In pursuit of the <span className="light-blue-text text-lighten-2">magic</span> behind <span class="underline">imperfection</span></h2>
+            <h2 className="white-text">In pursuit of the <span className="light-blue-text text-lighten-2">magic</span> behind <span className="underline">imperfection</span></h2>
         </div>
     );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,18 +2,18 @@ import React from "react";
 
 function About() {
   return (
-    <section id="about" class="section scrollspy">
-        <h3 class="blue-grey-text text-darken-3 light-blue lighten-4">ABOUT ME</h3>
-        <div class="container">
+    <section id="about" className="section scrollspy">
+        <h3 className="blue-grey-text text-darken-3 light-blue lighten-4">ABOUT ME</h3>
+        <div className="container">
             <p>
-                Feeling a nagging need for a career change, I decided to take a <span class="light-blue-text text-darken-1">180˚</span> pivot from veterinary medicine to the web developer field. I am currently enrolled in a full-stack bootcamp program through <a class="teal-text text-darken-1" href="https://www.trilogyed.com/" aria-label="link to Triology Education Services">Triology Education Services</a> at <a class="teal-text text-darken-1" href="https://bootcamp.tlcenter.wustl.edu/" aria-label="Link to Washington University Saint Louis Bootcamp Program">Washington University</a> here in Saint Louis, Missouri.
+                Feeling a nagging need for a career change, I decided to take a <span className="light-blue-text text-darken-1">180˚</span> pivot from veterinary medicine to the web developer field. I am currently enrolled in a full-stack bootcamp program through <a className="teal-text text-darken-1" href="https://www.trilogyed.com/" aria-label="link to Triology Education Services">Triology Education Services</a> at <a className="teal-text text-darken-1" href="https://bootcamp.tlcenter.wustl.edu/" aria-label="Link to Washington University Saint Louis Bootcamp Program">Washington University</a> here in Saint Louis, Missouri.
             </p>
             <p>    
-                I absorb with facination every new skill learned and every new perspective gained. With discoveries of a passion behind <span class="light-blue-text text-darken-1">human-centric design</span> and <span class="light-blue-text text-darken-1">community-driven projects</span>, I look forward to creating that which can bring about real changes in the world. My meticulous attention to detail and organization serves as a reliable resource for efficient and clean work.
+                I absorb with facination every new skill learned and every new perspective gained. With discoveries of a passion behind <span className="light-blue-text text-darken-1">human-centric design</span> and <span className="light-blue-text text-darken-1">community-driven projects</span>, I look forward to creating that which can bring about real changes in the world. My meticulous attention to detail and organization serves as a reliable resource for efficient and clean work.
             </p>
             <blockquote>
                 <h5>Persistence in a ever changing world:</h5>
-                <h6>I won't forget where I started - but I'm <span class="light-blue lighten-4">far more eager</span> to look at where I want to go.</h6>
+                <h6>I won't forget where I started - but I'm <span className="light-blue lighten-4">far more eager</span> to look at where I want to go.</h6>
 
             </blockquote>
             <p>
